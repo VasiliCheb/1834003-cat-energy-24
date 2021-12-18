5838efc277dc7021155556441630787e43f6d5fa
VasiliCheb <skimanvasia@gmail.com>
2021-12-18 22:20:37 +0300

    Декоративные элементы главной страницы. Перестроение мелкого контента.

--- source/js/nav-toggle.js
+++ source/js/nav-toggle.js
@@ -1,14 +1,15 @@
-var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".main-nav__toggle");
 
-  navMain.classList.remove('main-nav--nojs');
+  navMain.classList.remove("main-nav--nojs");
+  navMain.classList.add("main-nav--closed");
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
+  navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("main-nav--closed")) {
+      navMain.classList.remove("main-nav--closed");
+      navMain.classList.add("main-nav--opened");
     } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add("main-nav--closed");
+      navMain.classList.remove("main-nav--opened");
     }
   });
